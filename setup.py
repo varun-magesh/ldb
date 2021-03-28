2a399457ba332ef35ccf3f51aebc52f36013d72c
Varun Iyer <varun_iyer@posteo.net>
2021-03-27 18:10:01 -0700

    Added pdf text tools to setuptools

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='ldb',
       version='0.1',
       description='Reference manager',
-      install_requires=['click', 'simple-term-menu', 'pybtex', 'fuzzywuzzy', 'levenshtein'],
+      install_requires=['click', 'simple-term-menu', 'pybtex', 'fuzzywuzzy', 'levenshtein', 'pdftotext', 'ocrmypdf'],
       py_modules=['ldb'],
       url='http://github.com/varun-iyer/ldb',
       author='Varun Iyer',
