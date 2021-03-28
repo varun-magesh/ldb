29a752e64d8d8d7829b459f4ec72e6f3588a88ed
Varun Iyer <varun_iyer@posteo.net>
2021-03-27 19:47:16 -0700

    Add whoosh to dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='ldb',
       version='0.1',
       description='Reference manager',
-      install_requires=['click', 'simple-term-menu', 'pybtex', 'fuzzywuzzy', 'levenshtein', 'pdftotext', 'ocrmypdf'],
+      install_requires=['click', 'simple-term-menu', 'pybtex', 'fuzzywuzzy', 'levenshtein', 'pdftotext', 'ocrmypdf', 'whoosh'],
       py_modules=['ldb'],
       url='http://github.com/varun-iyer/ldb',
       author='Varun Iyer',
